9c5847462d01145eadd73439c3b93b00b5cc685a
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2025-03-26 11:02:56 -0400

    feat: Add Routing & MDX Support (#6)
    * feat: Add Routing & MDX Test Page
    
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
    
    * feat: Add MDX Support
    
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
    
    ---------
    
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <></>;
+    return <p>Hello, World!</p>
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import mdx from '@mdx-js/rollup'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), mdx()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
