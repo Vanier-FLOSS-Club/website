00409564bfcdc45b5b73f45ea995c009aa6c7d52
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2025-03-30 20:16:59 -0400

    feat: Let desktop and mobile navbars share a list of links
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

---
+++ src/components/NavLinks.tsx
@@ -0,0 +1,8 @@
+export function navLinks() {
+  return [
+    { name: 'Home', href: '/' },
+    { name: 'MDX Test', href: '/mdx-test' },
+    { name: 'About', href: '/about' },
+    { name: 'Projects', href: '/projects' },
+  ];
+}
