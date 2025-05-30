8efb60ee99b7b96d6c29b343594afc886518e57d
Zhang Y.D. <cnfighteraimi@gmail.com>
2025-05-30 00:12:47 -0400

    Update: Combined auto-events generated .mjs file

---
+++ .vitepress/theme/assets/eventData.mjs
@@ -0,0 +1,17 @@
+// Upcoming events data
+const linkData = [
+  {
+    type: "events",
+    typeName: "Upcoming: ",
+    typeDesc: "Conferences, Meetups, and Workshops",
+    typeList: [
+      {
+        name: "Test #2",
+        desc: "Test JSON generator again\n\nDate: 2025-05-29",
+        time: "2025-05-29"
+      }
+    ]
+  }
+];
+
+export default linkData;
