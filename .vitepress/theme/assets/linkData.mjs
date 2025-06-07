34c65d43e94d7c6c32d53f446dda673cef013fa5
Zhang Y.D. <cnfighteraimi@gmail.com>
2025-06-07 13:23:43 -0400

    fix: Restore Friend Link List

--- .vitepress/theme/assets/linkData.mjs
+++ .vitepress/theme/assets/linkData.mjs
@@ -5,6 +5,12 @@ const linkData = [
     typeName: "Friends: ",
     typeDesc: "Great partners and friends of the Vanier FLOSS Club",
     typeList: [
+      {
+        name: "Test",
+        avatar: "",
+        desc: "This is a test friend component",
+        url: "https://github.com/iXORTech",
+      }
     ],
   }
 ];
